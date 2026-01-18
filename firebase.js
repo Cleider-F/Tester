276cc252bd1da6f680384f59e8414c8e0b18a13f
Cleider-F <cleider1000@hotmail.com>
2026-01-18 02:16:40 -0300

    Criar firebase.js

---
+++ firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, enableIndexedDbPersistence } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+enableIndexedDbPersistence(db).catch(() => {});
+
+export { db };
